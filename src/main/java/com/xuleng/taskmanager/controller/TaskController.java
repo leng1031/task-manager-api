e6c505eb7d1cf114ff9cb7bd49168523536954b2
leng0013 <xu.leng@outlook.com>
2026-03-27 19:44:06 -0400

    implement update delete patch and pagination

--- src/main/java/com/xuleng/taskmanager/controller/TaskController.java
+++ src/main/java/com/xuleng/taskmanager/controller/TaskController.java
@@ -2,8 +2,10 @@ package com.xuleng.taskmanager.controller;
 
 import com.xuleng.taskmanager.common.ApiResponse;
 import com.xuleng.taskmanager.entity.Task;
+import com.xuleng.taskmanager.entity.TaskStatus;
 import com.xuleng.taskmanager.service.TaskService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -36,5 +38,31 @@ public class TaskController {
         return new ApiResponse<>(200,"success",taskService.getTaskById(id));
     }
 
+    @PutMapping("/{id}")
+    public ApiResponse<Task> updateTask(@PathVariable Long id, @Valid @RequestBody Task task){
+        return new ApiResponse<>(200,"updated",taskService.updateTask(id,task));
+    }
+
+    @DeleteMapping("/{id}")
+    public ApiResponse<Void> deleteTask (@PathVariable long id){
+        taskService.deleteTask(id);
+        return new ApiResponse<>(200,"deleted",null);
+    }
+
+    @PatchMapping("/{id}/status")
+    public ApiResponse<Task> updateStatus(
+            @PathVariable Long id,
+            @RequestParam TaskStatus status
+            ){
+        return new ApiResponse<>(200,"Status updated",taskService.updateStatus(id,status));
+    }
+
+    @GetMapping("/page")
+    public ApiResponse<Page<Task>> getTasks(
+            @RequestParam int page,
+            @RequestParam int size
+    ){
+        return new ApiResponse<>(200,"success", taskService.getTask(page, size));
+    }
 
 }
--- src/main/java/com/xuleng/taskmanager/service/TaskService.java
+++ src/main/java/com/xuleng/taskmanager/service/TaskService.java
@@ -4,6 +4,8 @@ import com.xuleng.taskmanager.entity.Task;
 import com.xuleng.taskmanager.entity.TaskStatus;
 import com.xuleng.taskmanager.exception.TaskNotFoundException;
 import com.xuleng.taskmanager.repository.TaskRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -38,4 +40,38 @@ public class TaskService {
         return taskRepository.findById(id)
                 .orElseThrow(() ->new TaskNotFoundException("Task not found"));
     }
+
+    //update Task by task id
+    public Task updateTask (Long id, Task newTask){
+        Task existing = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("task not found"));
+
+        existing.setTitle(newTask.getTitle());
+        existing.setDescription(newTask.getDescription());
+        //existing.setStatus(newTask.getStatus());
+        existing.setDueDate(newTask.getDueDate());
+
+        return taskRepository.save(existing);
+    }
+
+    //delete Task thru task id
+    public void deleteTask(Long id){
+        if(!taskRepository.existsById(id)){
+            throw new TaskNotFoundException("Task not found");
+        }
+        taskRepository.deleteById(id);
+    }
+
+    public Task updateStatus(Long id, TaskStatus status){
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException("Task not found"));
+
+        task.setStatus(status);
+        return taskRepository.save(task);
+    }
+
+    public Page<Task> getTask(int page, int size){
+        return taskRepository.findAll(PageRequest.of(page, size));
+    }
+
 }
